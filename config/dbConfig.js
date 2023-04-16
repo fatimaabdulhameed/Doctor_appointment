3a5aa3792d4fc1fd8199665d60cd3d4d43a92fce
unknown <fatimaabdulhameed>
2023-04-16 22:39:34 +0500

    firstcommit

---
+++ config/dbConfig.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+
+
+mongoose.connect(process.env.MONGO_URL);
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("MongoDB connection is successful");
+  console.log("all good");
+
+});
+
+connection.on("error", (error) => {
+  console.log("Error in MongoDB connection", error);
+});
+
+module.exports = mongoose;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+const userRoute = require("./routes/userRoute");
+const adminRoute = require("./routes/adminRoute");
+const doctorRoute = require("./routes/doctorsRoute");
+const path = require("path");
+mongoose.set('strictQuery', false);
+// mongoose.set('strictQuery', true);
+
+
+
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
